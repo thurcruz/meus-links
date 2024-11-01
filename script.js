00cef7482b36f414401ff452e865e2e9c1b40f58
thurcruz <arthurindcruz@gmail.com>
2024-10-31 21:41:06 -0300

    novo layuot

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', "./assets/logo-arthur-cruz(black).svg")
   } else {
     // se tiver sem light mode, manter a imagrm normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/logo-arthur-cruz(white).svg")
   }
 }
